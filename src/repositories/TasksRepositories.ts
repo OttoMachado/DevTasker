2758f7641789edf3ee150ae9700c1108b1b6d393
OttoMachado <158228292+OttoMachado@users.noreply.github.com>
2025-04-23 17:08:11 -0300

    Update TasksRepositories.ts
    adicionando comentarios

--- src/repositories/TasksRepositories.ts
+++ src/repositories/TasksRepositories.ts
@@ -4,11 +4,13 @@ import { Task } from "../models/Tasks";
 export class TaskRepository {
   private repo = AppDataSource.getRepository(Task);
 
+  //Cria a tarefa
   async createTask(title: string, description: string, status: string, deliveryDate: string, userId: number) {
     const task = new Task(title, description, status, deliveryDate, userId);
     return await this.repo.save(task);
   }
 
+  //Busca por ID
   async findTaskById(id: number) {
     return await this.repo.findOne({ 
       where: { id }, 
@@ -16,6 +18,7 @@ export class TaskRepository {
     });
   }
 
+  //Esse metodo aqui Busca pelo ID do usuario
   async findTasksByUserId(userId: number) {
     return await this.repo.find({ 
       where: { user: { id: userId } },
@@ -23,6 +26,7 @@ export class TaskRepository {
     });
   }
 
+  // Esse da update na task
   async updateTask(id: number, fields: Partial<Task>) {
     const task = await this.findTaskById(id);
     if (!task) return null;
@@ -30,12 +34,14 @@ export class TaskRepository {
     return await this.repo.save(task);
   }
 
+  // Esse deleta a task
   async deleteTask(id: number) {
     const task = await this.findTaskById(id);
     if (!task) return null;
     return await this.repo.remove(task);
   }
 
+  // Busca todas as Tasks
   async findAllTasks() {
     return await this.repo.find({ relations: ["user"] });
   }
